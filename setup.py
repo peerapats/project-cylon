1911189a75e3cb8f8d8c144cc5b963107e77d1a6
Peerapat <Peerapat@MacBook.local>
2014-05-14 11:49:59 +0700

    update to v0.2.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='project-cylon',
-      version='0.2.2',
+      version='0.2.3',
       description='Web automated testing framework using behave and selenium',
       long_description=
       """
